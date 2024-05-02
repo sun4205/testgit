93e47fcfbaa41a1b222510d7fae2a3bef146fb9f
sun4205 <hanna4205@gmail.com>
2024-05-02 02:53:12 -0400

    practice javascript

--- practice.js
+++ practice.js
@@ -55,3 +55,66 @@ console.log('--------------')
 return(width>height);
  
  }
+
+ console.log(isLandscape(300,600));
+
+ function fizzBuzz(input){
+  
+  if((input % 3===0 )&& (input % 5 ===0)){
+    return 'Fizz'
+  }else if (input % 5 ===0){
+    return'Buzz'
+  }else if (input % 3 ===0 ){
+    return 'fizzBuzz'
+  }else if(typeof input !== 'number'){
+    return'Not a number';
+  }
+  return input;
+ }
+
+ let output = fizzBuzz(false);
+ console.log(output);
+
+ function checkSpeed(speed){
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+
+  if(speed< speedLimit + kmPerPoint){
+    console.log('ok');
+  } else {
+    const point = Math.floor((speed-speedLimit)/kmPerPoint);
+    if(point >=12)
+    console.log('Liscense suspended');
+  else
+    console.log('point', point);
+  }
+ }
+
+ checkSpeed(75);
+
+ function showNumbers(limit){
+  for(let i =0;  i<=limit; i++){
+    if(i % 2 === 0 ){
+      console.log(i,'EVEN');
+    } else{
+      console.log(i,'ODD');
+    }
+  }
+ }
+
+ console.log(showNumbers(10));
+
+ const array = [0,null,undefined,'',2,3]
+
+ function countTruthy(array){
+  let count=0;
+  for(let value of array){
+    if(value){
+      count++;
+    }
+
+      }
+      return count;
+ }
+
+ console.log(countTruthy(array));
