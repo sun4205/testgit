19f68771c691a0944ac33af5884bbd0973ca0f17
sun4205 <hanna4205@gmail.com>
2024-05-06 21:51:48 -0400

    practice javascript

--- practice.js
+++ practice.js
@@ -270,3 +270,29 @@
  }
 
  let makeAddress = new Address('a','b','c');
+
+ const numbers = [1,2,3,1,4];
+
+ console.log(numbers.indexOf('a'));
+ console.log(numbers.indexOf(1));
+ console.log(numbers.lastIndexOf(1));
+ console.log(numbers.indexOf(1,2));
+
+ const courses = [
+  {id:1, name:'a'},
+  {id:2, name:'b'}
+ ];
+
+ const course = courses.find(function(course){
+  return course.name === 'a';
+ });
+
+ console.log(course);
+
+ const examNum = [1, 2, 3, 4];
+
+ const last = numbers.pop();
+ console.log(examNum);
+ console.log(last);
+
+ 
