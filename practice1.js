1faa4d791442b06badaa981a9fcdf28a0c360239
sun4205 <hanna4205@gmail.com>
2024-05-16 17:04:50 -0400

    practice js

--- practice1.js
+++ practice1.js
@@ -171,3 +171,109 @@ const person = {
 
 
     }
+
+    showNumbers(10);
+
+    function showNumbers(limit){
+        for (let i=0; i<=10; i++){
+            if([i] % 2 ===0){
+                console.log(i,'even');
+            }else{
+                
+                    console.log(i,'odd');
+                
+            }
+        }
+    }
+
+    const array = [0,null,undefined,"",2,3];
+
+    console.log(countTruthy(array));
+
+    function countTruthy(array){
+        const count = 0;
+        for(let value of array)
+        if(value)
+        count++;
+    return;
+    }
+
+    const movie = {
+        title : 'a',
+        releaseYear : 2018,
+        rating : 4.5,
+        director : 'b'
+    };
+
+    showProperties(movie);
+
+    function showProperties(ob){
+        for(let key in obj){
+            if(typeof obj[key] ==='string')
+            console.log(key,obj[key]);
+        }
+    }
+
+    console.log(sum(10));
+
+    function sum(limit){
+        let sum = 0;
+        for(let i=0; i<=limit; i++)
+            if(i % 3 ===0 ||i % 5 ===0)
+            sum += i;
+            
+            return sum;
+            
+    }
+
+    const marks = [80, 80, 50];
+
+    console.log(calculateGrade(marks));
+
+    function calculateGrade(marks){
+        const average = calculateAvarage(marks);
+        if(averagy <60) return 'F';
+        if(average <70) return 'D';
+        if(average <80) return 'C';
+        if(averagy <90) return 'B';
+        return 'A';
+
+
+    }
+    
+
+    function calculateAvarage(array){
+        const sum = 0;
+        for(let value of array)
+            sum += value;
+         
+        return sum/array.length;
+    }
+
+    showStarts(5);
+
+    function showStars(rows) {
+        for(let row = 1; row<=rows; row++){
+            let pattern = '';
+            for(let i = 0; i< row; i++)
+                pattern += '*';
+            console.log(pattern);
+        }
+    }
+
+
+    showPrimes(20);
+
+    
+    function showPrimes(limit){
+        for(let number = 2; number<=limit; number++)
+            if(isPrime(number)) console.log(number);
+    }
+
+    function isPrime(number){
+        for(let factor =2; factor<number; factor++)
+            if(number % factor ===0)
+                 return false;
+
+                 return true;
+    }
